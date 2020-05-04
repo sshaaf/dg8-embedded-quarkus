4de34c0087081948ea8364b796a2228be9608057
sshaaf <sshaaf@redhat.com>
2020-05-04 20:01:42 +0200

    remove injection, that is not used

--- src/main/java/org/acme/ScoreService.java
+++ src/main/java/org/acme/ScoreService.java
@@ -34,7 +34,6 @@ public class ScoreService {
 
     Logger log = LoggerFactory.getLogger(ScoreService.class);
 
-    @Inject
     EmbeddedCacheManager cacheManager;
 
     public List<Score> getAll() {
